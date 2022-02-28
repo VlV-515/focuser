a3b2b732ef6d19cba4232b354ff1d201447c1eec
VlV <92279938+VlV-515@users.noreply.github.com>
2022-02-28 14:32:06 -0600

    feat➕: Add task service logic

---
+++ src/services/task.service.ts
@@ -0,0 +1,23 @@
+import { TaskInterface } from './../app/app.component';
+import { Injectable } from '@angular/core';
+import { Observable, Observer, Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  private tasks!: TaskInterface[];
+  private taskList = new Subject<TaskInterface[]>();
+
+  getTaskList(): Observable<TaskInterface[]> {
+    return this.taskList.asObservable();
+  }
+  addTask(task: TaskInterface): void {
+    this.tasks.push(task);
+    this.taskList.next(this.tasks);
+  }
+  deleteTask(index: number): void {
+    this.tasks.splice(index);
+    this.taskList.next(this.tasks);
+  }
+}
