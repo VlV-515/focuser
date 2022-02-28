f5355242b4977e785c80388bc109748a76096cec
VlV <92279938+VlV-515@users.noreply.github.com>
2022-02-28 14:11:00 -0600

    feat➕: Add catching ngForm ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
+import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
+
+export interface TaskInterface {
+  name: string;
+  minutes: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'focuser';
+  onSaveTask(form: TaskInterface): void {
+    if (!form.name) return window.alert('Add task name please. =)');
+    if (!form.minutes) return window.alert('Add task time please. =)');
+    console.table({ form });
+  }
 }
